c66c8236967b7df7d755237178e965b4839e6ddc
AmnaKhan666 <e87092700@gmail.com>
2024-05-17 10:36:36 +0500

    java script increment

---
+++ app.js
@@ -0,0 +1,6 @@
+var a = 2;
+var b = 10;
+// document.write (a++ + a); // 2 + 3 = 5
+// document.write (++a + a++ + a); // 3 + 3 + 4 = 10
+// document.write ( a++ + a + ++a); // 2 + 3 + 4 = 9
+document.write (--b + b-- + b) //9 + 9 + 8 = 26
